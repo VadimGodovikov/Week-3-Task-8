eebce1e560349605df845c2ac68878b795c91b82
VadimGodovikov <119358867+VadimGodovikov@users.noreply.github.com>
2022-12-16 02:54:05 +0300

    git add

---
+++ prog8/prog8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace prog8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			do
+			{
+				try
+				{
+					Console.WriteLine("Введите строку: ");
+					string str = Console.ReadLine();
+					var result = new Regex(@"\d+").Matches(str).Cast<Match>().Max(x => int.Parse(x.ToString()));
+					Console.WriteLine($"Максимальное число в строке: {result}");
+					Console.ReadKey(true);
+					break;
+				}
+				catch
+				{
+					Console.WriteLine("Строка должна состоять из целых чисел");
+				}
+			} while (true);
+			
+		}
+	}
+}
---
+++ prog8form/prog8form/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace prog8form
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				string str = textBox1.Text;
+				var result = new Regex(@"\d+").Matches(str).Cast<Match>().Max(x => int.Parse(x.ToString()));
+				string res = $"Ваша строка: {str}";
+				res += "" + Environment.NewLine;
+				res += $"Максимальное число в строке: {result}";
+				MessageBox.Show($"{res}", "Результат");
+			}
+			catch
+			{
+				MessageBox.Show("Строка должна состоять из целых чисел", "Ошибка");
+				return;
+			}
+		}
+	}
+}
